2b3afa40216c48f9df5ef5d6e0985c34597b470c
dipu <giyasuddin636@gamil.com>
2021-10-11 18:24:38 +0600

    dipu commit product image set from office

--- app/src/main/java/com/aait/oms/product/Product_Details_view_Activity.java
+++ app/src/main/java/com/aait/oms/product/Product_Details_view_Activity.java
@@ -1,10 +1,12 @@
 package com.aait.oms.product;
 
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,13 +16,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.aait.oms.R;
 import com.aait.oms.product.common.CommonFunction;
+import com.aait.oms.util.AppUtils;
+import com.aait.oms.util.SQLiteDB;
 import com.google.gson.Gson;
 
-public class Product_Details_view_Activity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product_Details_view_Activity extends AppCompatActivity implements View.OnClickListener {
 
     private  TextView prodname, prodcode,prodprice,proddetails ,prodstock;
     private ImageView prodimageview;
     private ImageButton addcard,favorite,feedback;
+
+    ProductModel prodmodel;
+    SQLiteDB sqLiteDB;
+
+    AppUtils appUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +55,14 @@ public class Product_Details_view_Activity extends AppCompatActivity {
         favorite = findViewById(R.id.productFavouritebottonId);
         feedback = findViewById(R.id.productfeedbackbottonId);
 
-        Gson gson = new Gson();
-        ProductModel prodmodel = gson.fromJson(getIntent().getStringExtra("product"), ProductModel.class);
-   /*     CommonFunction commonFunction = new CommonFunction(this);
-        long uodid = Math.round(Float.parseFloat(prodmodel.uomid));
-        String stringid = String.valueOf(uodid);
-        String uomname = commonFunction.getuomname(Integer.parseInt(stringid));*/
+        sqLiteDB = new SQLiteDB(this);
+        appUtils = new AppUtils(this);
+        addcard.setOnClickListener(this);
+        favorite.setOnClickListener(this);
+        feedback.setOnClickListener(this);
 
+        Gson gson = new Gson();
+        prodmodel = gson.fromJson(getIntent().getStringExtra("product"), ProductModel.class);
         prodname.setText(prodmodel.getProductname());
         prodcode.setText("Code : "+prodmodel.getL4code());
         prodprice.setText(" TK. "+prodmodel.getSalesrate());
@@ -60,16 +73,6 @@ public class Product_Details_view_Activity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
         prodimageview.setImageBitmap(bitmap);
 
-
-
-        Gson gsonf = new Gson();
-        //System.out.println(prodmodel.toString());
-
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            Object product = bundle.get("product");
-
-        }
     }
 
 
@@ -81,5 +84,43 @@ public class Product_Details_view_Activity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.productFavouritebottonId:
+               Cursor cursor = sqLiteDB.getallfavoriteProduct();
+                List<String> productlist = new ArrayList<>();
+               if (cursor.moveToFirst()){
+                   productlist.add(cursor.getString(1));
+                   for (int i = 0 ; i<productlist.size(); i++){
+                       if (productlist.get(i) == prodmodel.l4code){
+
+                           appUtils.appToast("This Product already added as your favorite Product ");
+                       }
+                       else {
+                           sqLiteDB.insertProduct(prodmodel.getL4code());
+                           appUtils.appToast("A New Product added as your favorite Product");
+                       }
+
+                   }
+               }else {
+                   sqLiteDB.insertProduct(prodmodel.getL4code());
+                   appUtils.appToast("A New Product added as your favorite Product");
+               }
+
+
+            //    sqLiteDB.insertProduct(prodmodel.getL4code());
+
+
+                break;
+            case R.id.cardproductAddtTocartbottonId:
+                break;
+            case R.id.productfeedbackbottonId:
+                break;
+
+        }
+
     }
 }
